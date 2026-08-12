d04b440a4492e5547efa48890f43cf9dcd8957ef
Dipika K.C <dipikak0323@gmail.com>
2026-08-12 13:04:42 +0545

    feat: Blog controller

--- apps/api/src/controllers/blog.controller.ts
+++ apps/api/src/controllers/blog.controller.ts
@@ -1,5 +1,6 @@
+import { uploadFile } from "@funtush/storage";
 import type { Request, Response } from "express";
-import { createCategoryService, getCategoriesService, updateCategoryService } from "src/services/blog.service";
+import { createBlogService, createCategoryService, getBlogsService, getCategoriesService, updateBlogService, updateCategoryService } from "src/services/blog.service";
 
 export const createcategory = async (
     req: Request,
@@ -73,6 +74,107 @@ export const getAgencycategories = async (
             success: true,
             count: categories.length,
             data: categories,
+        });
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            message:
+                err instanceof Error
+                    ? err.message
+                    : "Something went wrong",
+        });
+    }
+};
+
+export const createBlog = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        const agencyUserId = req.tenantId as string;
+
+        const photos = (req.files as Express.Multer.File[]) || [];
+
+        const urls = await Promise.all(
+            photos.map((photo) => uploadFile(photo))
+        );
+
+        const blog = await createBlogService(
+            agencyUserId,
+            {
+                ...req.body,
+                photos: urls,
+            }
+        );
+
+        return res.status(201).json({
+            success: true,
+            data: blog,
+        });
+
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            message:
+                err instanceof Error
+                    ? err.message
+                    : "Something went wrong",
+        });
+    }
+};
+
+export const updateBlog = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        const agencyUserId = req.tenantId as string;
+        const blogId = req.params.id as string;
+
+        const photos = (req.files as Express.Multer.File[]) || [];
+
+        const urls = await Promise.all(
+            photos.map((photo) => uploadFile(photo))
+        );
+
+        const blog = await updateBlogService(
+            agencyUserId,
+            blogId,
+            {
+                ...req.body,
+                photos: urls,
+            });
+
+        return res.status(200).json({
+            success: true,
+            data: blog,
+        });
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            message:
+                err instanceof Error
+                    ? err.message
+                    : "Something went wrong",
+        });
+    }
+};
+
+export const getAgencyBlogs = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        const agencyUserId = req.tenantId as string;
+
+        const blogs = await getBlogsService(
+            agencyUserId
+        );
+
+        return res.status(200).json({
+            success: true,
+            count: blogs.length,
+            data: blogs,
         });
     } catch (err) {
         return res.status(400).json({
